8ff7e98c1af90b91886d841e33d7930b05e59501
Seth Frutiger <sethfrutiger@gmail.com>
2022-09-11 11:42:41 -0400

    added production database

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,25 @@
-const mongoose = require('mongoose');
-const colors = require('colors');
+const mongoose = require("mongoose");
+const colors = require("colors");
 
 const connectDB = async () => {
-    try {
-        const connect = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB connected: ${connect.connection.host}`.cyan);
-    } catch (error) {
-        console.log(error);
-        process.exit(1)
+  try {
+    if (process.env.NODE_ENV === "development") {
+      const connect = await mongoose.connect(process.env.MONGO_URI_DEVELOPMENT);
+      console.log(
+        `MongoDB connected to development server: ${connect.connection.host}`
+          .cyan
+      );
+    } else if (process.env.NODE_ENV === "production") {
+      const connect = await mongoose.connect(process.env.MONGO_URI_PRODUCTION);
+      console.log(
+        `MongoDB connected to production server: ${connect.connection.host}`
+          .cyan
+      );
     }
-}
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
