2183aebdd7291d7dedd41ab33e64a03e8f416959
Seth Frutiger <sethfrutiger@gmail.com>
2022-09-13 12:01:25 -0400

    service account fixes

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,55 +1,9 @@
 const admin = require("firebase-admin");
 
-/* let projectID;
-let privateKeyID;
-let privateKey;
-let clientEmail;
-let clientID;
-let authURI;
-let tokenURI;
-let authProvider;
-let clientCertURL;
-
-if (process.env.NODE_ENV === "development") {
-  projectID = process.env.FB_DEVELOPMENT_PROJECT_ID;
-  privateKeyID = process.env.FB_DEVELOPMENT_PRIVATE_KEY_ID;
-  privateKey = process.env.FB_DEVELOPMENT_PRIVATE_KEY;
-  clientEmail = process.env.FB_DEVELOPMENT_CLIENT_EMAIL;
-  clientID = process.env.FB_DEVELOPMENT_CLIENT_ID;
-  authURI = process.env.FB_DEVELOPMENT_AUTH_URI;
-  tokenURI = process.env.FB_DEVELOPMENT_TOKEN_URI;
-  authProvider = process.env.FB_DEVELOPMENT_AUTH_PROVIDER_X509_CERT_URL;
-  clientCertURL = process.env.FB_DEVELOPMENT_CLIENT_X509_CERT_URL;
-} else if (process.env.NODE_ENV === "production") {
-  projectID = process.env.FB_PRODUCTION_PROJECT_ID;
-  privateKeyID = process.env.FB_PRODUCTION_PRIVATE_KEY_ID;
-  privateKey = process.env.FB_PRODUCTION_PRIVATE_KEY;
-  clientEmail = process.env.FB_PRODUCTION_CLIENT_EMAIL;
-  clientID = process.env.FB_PRODUCTION_CLIENT_ID;
-  authURI = process.env.FB_PRODUCTION_AUTH_URI;
-  tokenURI = process.env.FB_PRODUCTION_TOKEN_URI;
-  authProvider = process.env.FB_PRODUCTION_AUTH_PROVIDER_X509_CERT_URL;
-  clientCertURL = process.env.FB_PRODUCTION_CLIENT_X509_CERT_URL;
-} else {
-  console.log("environment not set");
-} */
-
 const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert(
-    serviceAccount
-    /*     type: "service_account",
-    project_id: projectID,
-    private_key_id: privateKeyID,
-    private_key: privateKey,
-    client_email: clientEmail,
-    client_id: clientID,
-    auth_uri: authURI,
-    token_uri: tokenURI,
-    auth_provider_x509_cert_url: authProvider,
-    client_x509_cert_url: clientCertURL, */
-  ),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 function auth(req, res, next) {
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
