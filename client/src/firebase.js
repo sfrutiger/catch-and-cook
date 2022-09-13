61752b28f1d1ffc6da5bf9f1dbb9a99dfcc1dec7
Seth Frutiger <sethfrutiger@gmail.com>
2022-09-13 12:16:58 -0400

    firebase troubleshoot

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -7,19 +7,10 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-let firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_DEVELOPMENT_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBAS_DEVELOPMENT_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_DEVELOPMENT_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_DEVELOPMENT_STORAGEBUCKET,
-  messagingSenderId:
-    process.env.REACT_APP_FIREBASE_DEVELOPMENT_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FIREBASE_DEVELOPMENT_APPID,
-  measurementId: process.env.REACT_APP_FIREBASE_DEVELOPMENT_MEASUREMENTID,
-};
+let firebaseConfig = {};
 const environment = process.env.NODE_ENV;
 
-/* if (environment === "development") {
+if (environment === "development") {
   firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_DEVELOPMENT_API_KEY,
     authDomain: process.env.REACT_APP_FIREBAS_DEVELOPMENT_AUTHDOMAIN,
@@ -43,7 +34,7 @@ const environment = process.env.NODE_ENV;
   };
 } else {
   console.log("environment not set");
-} */
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
